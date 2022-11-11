6af69ede4ada328e0dd847fa42e7dc48e06d41a3
Oleg Gnatyk <103590840+GnatykOleg@users.noreply.github.com>
2022-11-11 20:25:49 +0200

    Update project
    Deleted unnecessary files/comments/code.
    Change readme.

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -4,6 +4,8 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import s from './Searchbar.module.css';
 
+import Container from '../Container/Container';
+
 export default class Searchbar extends Component {
   state = {
     value: '',
@@ -30,21 +32,23 @@ export default class Searchbar extends Component {
   render() {
     const { value } = this.state;
     return (
-      <header className={s.searchbar}>
-        <form className={s.form} onSubmit={this.handleSubmit}>
-          <input
-            onChange={this.handleInputChange}
-            value={value}
-            className={s.input}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-          <button type="submit" className={s.button}>
-            Search
-          </button>
-        </form>
+      <header className={s.header}>
+        <Container>
+          <form className={s.form} onSubmit={this.handleSubmit}>
+            <input
+              onChange={this.handleInputChange}
+              value={value}
+              className={s.input}
+              type="text"
+              autoComplete="off"
+              autoFocus
+              placeholder="Please type text...."
+            />
+            <button type="submit" className={s.button}>
+              Search
+            </button>
+          </form>
+        </Container>
       </header>
     );
   }
